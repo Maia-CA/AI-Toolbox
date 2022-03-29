dd6ee314d4f0dee0efa6ba14705494f6dbb31faf
Maia-CA <mtbiker360@gmail.com>
2022-03-29 03:31:14 -0700

    first commit

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,13 @@
+def Settings( **kwargs ):
+  return {
+    'flags': [
+        '-x', 'c++',
+        '-std=c++2a',
+        '-Wall', '-Wextra', '-Werror',
+        '-DAI_LOGGING_ENABLED',
+        '-Iinclude/',
+        '-Itest/',
+        '-isystem/usr/include/eigen3/',
+        '-isystem/usr/include/python3.8/'
+    ],
+  }
